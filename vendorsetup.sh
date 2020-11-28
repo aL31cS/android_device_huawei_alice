b091be785a4a442d384b9e6ad559b6553340c4e0
Alexandru Chimac <36740194+aL31cS@users.noreply.github.com>
2020-11-28 12:51:29 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,7 @@
+# Copyright 2018 The Maru OS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 add_lunch_combo lineage_alice-userdebug
+
+add_lunch_combo maru_alice-userdebug
